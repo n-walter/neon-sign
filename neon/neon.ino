1b570a6d59e2e837256831f1d82908be4795e43d
Nicholas Walter <github@nicholaswalter.de>
2025-10-12 12:52:53 +0200

    added second button

--- neon/neon.ino
+++ neon/neon.ino
@@ -80,6 +80,8 @@ class ColourPair {
     }
 };
 
+
+
 class Section {
     public:
     // colour stuff
@@ -249,6 +251,53 @@ class Section {
     }
 };
 
+class InnerSection: public Section {
+    public: 
+    InnerSection(Colour a1, Colour a2, Colour b1, Colour b2, Colour c1, Colour c2, int stripLength, Adafruit_NeoPixel strip) : Section(a1, a2, b1, b2, c1, c2, stripLength, strip) {
+        
+    }
+
+
+    void animate() {
+        // I want my f-strings back. Fuck C++, all my homies hate C++
+        char strBuf[50];
+        sprintf(strBuf, "Animation step: %d/%d", animationStep, animationMaxStep);
+        Serial.println(strBuf);
+
+        switch (modeSelection) {
+        case 0:
+            doAnimateSolid(pair_a, false);
+            break;
+        case 1:
+            doAnimateSolid(pair_a, true);
+            break;
+        case 2:
+            doAnimateAlternating(pair_a);
+            break;
+        case 3: 
+            doAnimateGradientSlide(pair_a);
+            break;
+        case 4:
+            doAnimateRandomALl();
+            break;
+        case 5:
+            doAnimateRandomSingle();
+            break;
+        default:
+            Serial.println("\tdefault case");
+            // if we don't have a handler for this mode, reset to base
+            // I don't know how to create a list of function references in C++ and I don't want
+            // to constantly update a modulo value while adding new stuff
+            modeSelection = 0;
+            animate();
+            break;
+        }
+
+        animationStep = (animationStep + 1) % animationMaxStep;
+    }
+};
+
+
 // https://colorcodes.io/neon-color-codes/
 Colour NEON_RED = Colour(210, 39, 48);
 Colour NEON_WHITE = Colour(255, 255, 255);
@@ -278,7 +327,7 @@ Section border = Section(NEON_RED, NEON_WHITE, NEON_BLUE, NEON_PURPLE, NEON_GREE
 // setup for buttons and dials
 const byte innerButtonPin = 0;
 const byte borderButtonPin = 1;
-int animationDelay = 100;
+const int animationDelay = 20;
 
 
 void setup() {
@@ -296,8 +345,8 @@ void setup() {
     pinMode(innerButtonPin, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(innerButtonPin), innerButtonInterrupt, RISING); // trigger interrupt when button is released
     
-    // pinMode(borderButtonPin, INPUT_PULLUP);
-    // attachInterrupt(digitalPinToInterrupt(borderButtonPin), borderButtonInterrupt, RISING);
+    pinMode(borderButtonPin, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(borderButtonPin), borderButtonInterrupt, RISING);
 
     Serial.println("setup end");
 }
@@ -314,9 +363,8 @@ void loop() {
 void innerButtonInterrupt() {
     krumme.nextAnimation();
     gemeinde.nextAnimation();
-    border.nextAnimation();
 }
 
-// void borderButtonInterrupt() {
-//     // TODO: border.strip.setBrightness(0);
-// }
+void borderButtonInterrupt() {
+    border.nextAnimation();
+}
